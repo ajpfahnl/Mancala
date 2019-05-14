7bb6de900246c467b1b38f822fb615f661266c60
Arnold Pfahnl <ajpfahnl@gmail.com>
2019-05-13 17:18:57 -0700

    Initial Commit

---
+++ __CS32_Project_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  __CS32_Project_3
+//
+//  Created by Arnold Pfahnl on 5/13/19.
+//  Copyright © 2019 Arnold Pfahnl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
